73f47fd2e0d265b19ff126af341560068459e9af
Alex Vanderbist <alex.vanderbist@gmail.com>
2022-01-05 22:04:13 +0100

    Formatting

--- src/components/context/sections/Cookies.tsx
+++ src/components/context/sections/Cookies.tsx
@@ -1,12 +1,10 @@
-import React, {useContext} from 'react';
-import ContextList from "../ContextList";
-import {getContextValues} from "../../../util";
-import ErrorOccurrenceContext from "../../../contexts/ErrorOccurrenceContext";
+import React, { useContext } from 'react';
+import ContextList from '../ContextList';
+import { getContextValues } from '../../../util';
+import ErrorOccurrenceContext from '../../../contexts/ErrorOccurrenceContext';
 
 export default function Cookies() {
     const errorOccurrence = useContext(ErrorOccurrenceContext);
 
-    return (
-        <ContextList items={getContextValues(errorOccurrence, 'cookies')} />
-    )
+    return <ContextList items={getContextValues(errorOccurrence, 'cookies')} />;
 }
