70f90c2acffb7b7c63ea8e923d1b69d973b26ee4
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-04-09 14:34:57 +0200

    Update types

--- types/components/ui/EditorLink.d.ts
+++ types/components/ui/EditorLink.d.ts
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react';
 =======
 /// <reference types="react" />
 >>>>>>> 0626d75 (Types?)
+=======
+import React from 'react';
+>>>>>>> 4f883d5 (Update types)
 type Props = {
     path: string;
     lineNumber?: number;
